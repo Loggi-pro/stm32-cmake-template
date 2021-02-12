25a398ec5beb8c8d2e6a8c959369b08211c8093d
loggi <loggi.ain@gmail.com>
2021-02-12 12:52:09 +1000

    fix print usage for subdirectory cmake files

--- cmake/utils/print_size.cmake
+++ cmake/utils/print_size.cmake
@@ -44,7 +44,21 @@ function (print_size EXECUTABLE_NAME)
 	list(POP_BACK CMAKE_MESSAGE_CONTEXT)
 endfunction(print_size)
 
-function (print_size_all EXECUTABLE_NAME)
-	PRINT_SIZE_ALL_SOURCES(${EXECUTABLE_NAME})
+function (print_size_details EXECUTABLE_NAME)
+    set(OUTPUT_DIR ${EXECUTABLE_OUTPUT_PATH})
+    set(elf_file ${EXECUTABLE_NAME}.elf)
+    set(BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR})
+    get_target_property(SOURCES ${elf_file} SOURCES)
+    #get all sources, and make path to object files
+    list(TRANSFORM SOURCES PREPEND ${BUILD_DIR}/CMakeFiles/${elf_file}.dir/)
+    list(TRANSFORM SOURCES APPEND ".obj")
+    list(TRANSFORM SOURCES REPLACE "\\.\\./" "__/")
+    list(TRANSFORM SOURCES REPLACE "\\./" "_/")
+    #MESSAGE("OBJECT FILES = ${SOURCES}")
+    add_custom_command(TARGET ${EXECUTABLE_NAME}
+            POST_BUILD
+            COMMAND ${CMAKE_SIZE} ${SOURCES} > ${BUILD_DIR}/size_sources.txt)
+    set(PATH_TO_SIZE_UTILITY "${CURRENT_DIR}/../utils")
+    add_custom_command(TARGET ${EXECUTABLE_NAME} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} POST_BUILD COMMAND ${PATH_TO_SIZE_UTILITY}/print_size.cmd ${BUILD_DIR}/size_sources.txt)
 endfunction()
 
--- cmake/utils/print_size.cmd
+++ cmake/utils/print_size.cmd
@@ -3,6 +3,8 @@ SETLOCAL EnableDelayedExpansion
 rem echo %1
 set "spaces=                        "
 set "flashspaces=             "
+set /a TOTAL_FLASH=0
+set /a TOTAL_RAM=0
 echo	FILENAME                FLASH        RAM
 for /f "skip=1 tokens=1,2,3,6" %%A in (%1) do (  
 	set /a TEXT=%%A 
@@ -15,11 +17,15 @@ for /f "skip=1 tokens=1,2,3,6" %%A in (%1) do (
 	set PADDED=!NAME!%spaces%
 	set PADDEDF=!FLASH!%flashspaces%
 	echo !PADDED:~0,24!!PADDEDF:~0,13!!RAM!
+	set /a TOTAL_FLASH=!TOTAL_FLASH!+!FLASH!
+	set /a TOTAL_RAM=!TOTAL_RAM!+!RAM!
 	rem print only filename
 	rem echo.!name!	!FLASH!	!RAM!
-	
-	
 )
+echo _________________________________________
+set PADDEDF=!TOTAL_FLASH!%flashspaces%
+set PADDEDS=SUMMARY%spaces%
+echo !PADDEDS:~0,24!!PADDEDF:~0,13!!TOTAL_RAM!
 
 
 
